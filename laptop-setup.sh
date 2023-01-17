a69cb8ac464d2b331944a1308ef27cd525eec64f
Kamyar Ghasemlou <kamyar.ghasemlou@wolt.com>
2023-01-17 16:30:40 +0100

    soem small fixes + get and reuse email

--- laptop-setup.sh
+++ laptop-setup.sh
@@ -6,17 +6,16 @@ if ! command -v brew &> /dev/null; then
 	echo "Installing brew"
 	curl -o /tmp/homebrew-install.sh https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 	bash /tmp/homebrew-install.sh
-	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kamyarghasemlou/.zprofile
-	eval "$(/opt/homebrew/bin/brew shellenv)"
+	echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ~/.zprofile
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"	
 else 
 	echo "Brew exists, updating..."
 	brew update
 fi
 
-if ! command -v python3 &> /dev/null; then
-	brew install python@3
-	pip3 install --upgrade pip
-	pip3 install ansible
+if ! command -v ansible &> /dev/null; then
+	brew install ansible
 fi
 
 ansible-playbook main.yml --ask-become-pass
